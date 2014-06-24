db5bacd24888942c5e309edc47676568a67caab3
aalexeev239 <alexeev.andrey.a@gmail.com>
2014-06-24 14:47:58 +0400

    form page

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Phpted</title>
+  <title>PHPted</title>
   <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
   <!-- <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
   <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" /> -->
--- page-forms.php
+++ page-forms.php
@@ -1,6 +1,42 @@
 <?php 
-  require 'header.php' 
+  require 'header.php'
 ?>
+
+<section id="wrapper2">
+  <div id="featured" class="container">
+    <div class="box1">
+      <h2><span class="icon icon-table"></span>Простая форма</h2>
+      <form action="page-forms.php">
+        <p class="form-group">
+          <label for="field1">Введите что-нибудь:</label>
+          <input type="text" class="form-control"  name="field1" id="field1" value="test">
+        </p>
+        <p class="form-group">
+          <label for="email">Введите email:</label>
+          <input type="email" class="form-control"  name="email" id="email" value="test@djchsbch.com">
+        </p>
+        <p class="form-group">
+          <label for="text-message">И еще какую-нить фигню:</label>
+          <textarea class="form-control"  name="text-message" id="text-message">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos, blanditiis.</textarea>
+        </p>
+        <p class="form-group">
+          <button type="submit" class="btn submit">Отправить</button>
+        </p>
+      </form>
+    </div>
+    <div class="box2">
+      <h2><span class="icon icon-print"></span>Вывод:</h2>
+      <?php 
+       foreach ($_GET as $key => $value) {
+          $message[$key] = $value;
+          echo '<p class="form-group"><label>'.$key.':</label>
+          <strong>'.$value.'</strong>
+          </p>';
+        } 
+      ?>
+    </div>
+  </div>
+</section>
 
 <?php 
   require 'footer.php' 
