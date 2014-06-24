881ee5cc3eb0a73302d8d742fedaba0d557c5d21
aalexeev239 <alexeev.andrey.a@gmail.com>
2014-06-24 15:01:45 +0400

    Index added

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+  require 'header.php';
+ ?>
+<div id="wrapper1">
+  <div id="welcome" class="container">
+    <div class="title">
+      <h2>ДЗшки по PHP</h2>
+      <span class="byline">epicPHP 610</span> </div>
+    <div class="content">
+      <p>Get-запрос для формы</p>
+      <a href="page-forms.php" class="button">Смотреть</a></div>
+  </div>
+</div>
+<?php 
+  require 'footer.php';
+ ?>
