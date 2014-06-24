6e615d261b61dc5f104fa466042fc7fdb4b6da0b
aalexeev239 <alexeev.andrey.a@gmail.com>
2014-06-24 12:43:03 +0400

    Initial commit

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Phpted</title>
+  <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
+  <link href="default.css" rel="stylesheet" type="text/css" media="all" />
+  <link href="fonts.css" rel="stylesheet" type="text/css" media="all" />
+
+  <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
+</head>
+<body>
+  <div id="header-wrapper">
+  <div id="header" class="container">
+    <div id="menu">
+      <ul>
+        <li class="current_page_item"><a href="#" accesskey="1" title="">Mainpage</a></li>
+        <li><a href="#" accesskey="212" title="">Our Clients</a></li>
+        <li><a href="#" accesskey="3" title="">About Us</a></li>
+        <li><a href="#" accesskey="4" title="">Careers</a></li>
+        <li><a href="#" accesskey="5" title="">Contact Us</a></li>
+      </ul>
+    </div>
+    <div id="logo">
+      <h1><a href="#">PHPTED</a></h1>
+      <span>The dark side</span> </div>
+  </div>
+</div>
